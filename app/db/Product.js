c8226eaa7218d11bc8a9ad877db0bf99d45ea0f5
cbancu <bancu.cristi@gmail.com>
2018-03-04 20:34:34 +0200

    add mangoose schema

---
+++ app/db/Product.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var productSchema = new Schema({ 
+    name        : { type : String, required : true, unique : true }, 
+    category    : String,
+    category_id : Number,
+    calories    : { type:Number, required : true },
+    proteins    : { type:Number, required : true },
+    lipids      : { type:Number, required : true },
+    carbs       : { type:Number, required : true },
+    fibers      : { type:Number, required : true }
+});
+
+
+var Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
