c3237349ebeb9b8d4246899221d995d80f5bd430
cbancu <bancu.cristi@gmail.com>
2018-03-06 03:39:14 +0200

    parse all categories and store them in mongo

--- app/db/Product.js
+++ app/db/Product.js
@@ -2,15 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var productSchema = new Schema({ 
-    name        : { type : String, required : true, unique : true }, 
-    category    : String,
-    category_id : Number,
+    name        : { type : String, required : true}, 
+    category    : { type : String, required : true}, 
+    category_id : { type : Number, required : true}, 
     calories    : { type:Number, required : true },
     proteins    : { type:Number, required : true },
     lipids      : { type:Number, required : true },
     carbs       : { type:Number, required : true },
     fibers      : { type:Number, required : true }
 });
+
+productSchema.index({name : 1, category_id :1}, {unique : false});
 
 
 var Product = mongoose.model('Product', productSchema);
--- routes/categories.js
+++ routes/categories.js
@@ -1,55 +1,66 @@
-'use strict';
-
 const express = require('express');
 const router = express.Router();
 const CrawlCategories = require('../app/CrawlCategories');
 const Categories = require('../app/Categories');
 const ProductDbOps = require('../app/db/ProductDbOps');
+const Promise = require('promise');
 
 
 /* GET products for each category, serially */
 router.get('/', async (req, res, next) => {
-  const cc = new CrawlCategories();
   const categories = Categories;
-  for(let categ in categories) { 
-    console.log("getting data for " + categ.name);
-    let parsedData = await cc.getProductsPerCategory(categ);
-    console.assert(parsedData != null, "something went wrong when getting data for " + categ.name);
-  }
+
+  let results = await Promise.all(
+    categories.map( async (categ) => { 
+      console.log("getting data for :  " + categ.name);
+      await insertPerCategory(categ.name);
+      console.log("data inserted for : " + categ.name);
+    })
+  );
+  // for(let categ in categories) { 
+  //   console.log("getting data for " + categ.name);
+  //   let parsedData = await cc.getProductsPerCategory(categ);
+  //   console.assert(parsedData != null, "something went wrong when getting data for " + categ.name);
+  // }
   
-  res.send("OK - all categories");
+  res.send("OK - for all categories");
 });
 
 /* get only 1 category */
 router.get('/:category', async (req, res, next) => {
   const category  = req.params.category;
-  const cc = new CrawlCategories();
-  console.log("getting data for " + category);
-  let parsedData = await cc.getProductsPerCategory(category);
-  console.assert(parsedData != null, "something went wrong when getting data for " + category);
-  
-  const categories = Categories;
 
-  // try to find the category id 
-  let cat_id = -1; 
-  categories.forEach(e => {
-    if(e.name === category) { 
-      cat_id = e.id;
-    }
-  });
-
-  if(cat_id  == -1) { 
-    console.error('could not found category id for category ' + category);
-  }
-
-
-  console.info("INSERTING %d products for category %s", parsedData.length, category); 
-  await ProductDbOps.insertManyProducts(parsedData, category, cat_id);
+  await insertPerCategory(category);
 
   res.send( { 
    result :  "OK",
    category : category, 
    category_id : cat_id });
 });
+
+var insertPerCategory = async function(category) { 
+  const categories = Categories;
+  const cc = new CrawlCategories();
+
+  console.log("getting data for " + category);
+  
+  let parsedData = await cc.getProductsPerCategory(category);
+  
+  console.assert(parsedData != null, "something went wrong when getting data for " + category);
+
+   // try to find the category id 
+   let cat_id = -1; 
+   categories.forEach(e => {
+     if(e.name === category) { 
+       cat_id = e.id;
+     }
+   });
+ 
+   if(cat_id  == -1) { 
+     console.error('could not found category id for category ' + category);
+   }
+
+   await ProductDbOps.insertManyProducts(parsedData, category, cat_id);
+}
 
 module.exports = router;
